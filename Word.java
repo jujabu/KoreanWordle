e445b70e3fa5bac5572ea928b65e78be58e82770
jujabu <jakobos@uio.no>
2026-02-25 14:50:51 +0100

    Started on the Word-class. Can make an easy word with potential to get all the info about the word

---
+++ Word.java
@@ -0,0 +1,26 @@
+public class Word {
+
+    private int number;
+    private String word;
+    private String translation;
+
+    //My version 
+    public Word(int nr, String word, String translation) {
+
+        this.number = number;
+        this.word = word;
+        this.translation = translation;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getTranslation() {
+        return translation;
+    }
+}
