7ee60a3609179a18945af7f5cfbe45cc77df2789
jujabu <jakobos@uio.no>
2026-02-25 13:16:02 +0100

    Now it clicked for me that I make a Word- and WordList-class. Started a little on WordList

---
+++ WordList.java
@@ -0,0 +1,12 @@
+// A class that takes inn  a csv-file and converts it into a list/array of Word
+
+public class WordList() {
+
+    String csvFile;
+    Word[] wordList;
+
+    public WordList(String csvFile) {
+
+        this.csvFile = csvFile;
+    }
+}
